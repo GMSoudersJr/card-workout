4453b0bd3a4074881144824e247a9746a3553883
Gerald M. Souders Jr <gmsoudersjr@gmail.com>
2024-01-30 11:53:20 +0900

    Add functionality to rank widget

--- src/lib/components/RankInfoWidget.svelte
+++ src/lib/components/RankInfoWidget.svelte
@@ -18,14 +18,77 @@
 <div class="rank-info-widget">
   <ul>
   {#each ranks as rank}
-    <li>
+    <li class={`rank-${rank.symbol}`}>
+    {#if rank.count == 0}
+      <s class="strike-through">
       {rank.symbol}s: {rank.count}
+      </s>
+    {:else}
+      {rank.symbol}s: {rank.count}
+    {/if}
     </li>
   {/each}
   </ul>
 </div>
 
 <style>
+  .strike-through {
+    color: #D9D9D9;
+  }
+  ul {
+    display: grid;
+    grid-template-columns: repeat(3, max-content);
+    grid-template-rows: repeat(5, min-content);
+    column-gap: 1rem;
+    row-gap: 0.5rem;
+    justify-content: stretch;
+    justify-items: center;
+    grid-template-areas: 
+    "TWO SIX JACK"
+    "THREE SEVEN QUEEN"
+    "FOUR EIGHT KING"
+    "FIVE NINE ACE"
+    ". TEN . ";
+  }
+  .rank-2 {
+    grid-area: TWO;
+  }
+  .rank-3 {
+    grid-area: THREE;
+  }
+  .rank-4 {
+    grid-area: FOUR;
+  }
+  .rank-5 {
+    grid-area: FIVE;
+  }
+  .rank-6 {
+    grid-area: SIX;
+  }
+  .rank-7 {
+    grid-area: SEVEN;
+  }
+  .rank-8 {
+    grid-area: EIGHT;
+  }
+  .rank-9 {
+    grid-area: NINE;
+  }
+  .rank-10 {
+    grid-area: TEN;
+  }
+  .rank-J {
+    grid-area: JACK;
+  }
+  .rank-Q {
+    grid-area: QUEEN;
+  }
+  .rank-K {
+    grid-area: KING;
+  }
+  .rank-A {
+    grid-area: ACE;
+  }
   li {
     list-style: none;
   }
