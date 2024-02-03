0f1d605a87e53f85b1bdcdf384724295ffdea64d
Gerald M. Souders Jr <gmsoudersjr@gmail.com>
2024-02-04 00:14:16 +0900

    Resize suit info box again for mobile

--- src/lib/components/SuitInfoBox.svelte
+++ src/lib/components/SuitInfoBox.svelte
@@ -19,7 +19,7 @@
 <style>
   .suit-info-box {
     display: grid;
-    grid-template-columns: max-content 32px;
+    grid-template-columns: 1fr 1.618fr;
     grid-template-rows: max-content;
     border-radius: 4px;
     border: 1px solid #000080;
