73933277fd573cd5842d066dc79e7877e98f67d9
Gerald M. Souders Jr <gmsoudersjr@gmail.com>
2024-02-10 17:22:35 +0900

    Add a parameter for suitExercise to create a deck

--- src/functions/createDeckOfCards.ts
+++ src/functions/createDeckOfCards.ts
@@ -4,16 +4,28 @@ import { PlayingCard } from '../classes/playingCard'
 import type { TSuit } from '../types/suit';
 import type { TCardRank } from '../types/cardRank';
 import { createPlayingCard } from './createPlayingCard';
+import type { TSuitExercise } from '../types/suitExercise';
+import type {TExercise} from '../types/exercises';
 
-export const createDeckOfCards = () => {
+export const createDeckOfCards = (
+	aSelectionOfSuitExercises: TSuitExercise<TSuit>[] | undefined
+) => {
 	let result: PlayingCard[] = [];
 	const suits = Object.keys(ESuit);
 	const ranks = Object.keys(ECardRank);
 	for (let i = 0; i < suits.length; i++) {
 		let suit = suits[i];
+		let exercise: TExercise | undefined;
+		if (aSelectionOfSuitExercises) {
+			const thisSuitExercise = aSelectionOfSuitExercises.find((entry) => {
+				return entry.suit === suit;
+			});
+			exercise = thisSuitExercise?.exercise;
+		}
 		for (let j = 0; j < ranks.length; j++) {
 			let rank = ranks[j];
 			const playingCard = createPlayingCard(PlayingCard, rank as TCardRank, suit as TSuit);
+			playingCard.exercise = exercise;
 			result.push(playingCard);
 		}
 	}
