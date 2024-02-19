a54d45bad52bb96de203a70d7452105011a9387e
Gerald M. Souders Jr <gmsoudersjr@gmail.com>
2024-02-19 14:48:29 +0900

    Update more tests

--- tests/homePage.test.ts
+++ tests/homePage.test.ts
@@ -9,19 +9,20 @@ test('home page has expected h1', async ({ page }) => {
 });
 
 test.describe('hompage links are active and enabled', () => {
-	test('home page has link for about', async ({ page }) => {
+
+	test('About', async ({ page }) => {
 		const aboutLink = page.getByRole('link', { name: 'About' });
 		await expect(aboutLink).toBeVisible();
 		await expect(aboutLink).toBeEnabled();
 	});
 
-	test('home page has link for cards', async ({ page }) => {
+	test('Cards', async ({ page }) => {
 		const cardsLink = page.getByRole('link', { name: 'Cards' });
 		await expect(cardsLink).toBeVisible();
 		await expect(cardsLink).toBeEnabled();
 	});
 
-	test('home page has link for exercises', async ({ page }) => {
+	test('Exercises', async ({ page }) => {
 		const exercisesLink = page.getByRole('link', { name: 'Exercises' });
 		await expect(exercisesLink).toBeVisible();
 		await expect(exercisesLink).toBeEnabled();
