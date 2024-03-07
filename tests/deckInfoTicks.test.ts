8bab2f15166b96017cc702b34952e4afc7090238
Gerald M. Souders Jr <gmsoudersjr@gmail.com>
2024-03-07 21:52:57 +0900

    Update test to account for login

--- tests/deckInfoTicks.test.ts
+++ tests/deckInfoTicks.test.ts
@@ -2,15 +2,15 @@ import { expect, test, type Page, type Locator } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
 	await page.goto('/');
+	await page.waitForLoadState('domcontentloaded');
+	await page.getByRole('link', { name: 'Play' }).click();
 	await page.getByRole('link', { name: 'Cards' }).click();
 });
 
 const numberOfCards = 52;
 test.describe('check card tick', () => {
 	test('all ticks have same color before start', async ({ page }) => {
-		const deckRadioLabel = page.getByLabel('Deck', {exact: true});
 		const deckRadioButton = page.getByRole('radio', {name: 'Deck'});
-		await deckRadioLabel.check();
 		const allMiniCardTicks = await page.locator('.mini-card-tick').all();
 		await expect(deckRadioButton).toBeChecked();
 		expect(allMiniCardTicks).toHaveLength(52);
