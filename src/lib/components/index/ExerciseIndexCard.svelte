99e1196642152b21dc66ff0218d3c62e76a99c82
Gerald M. Souders Jr <gmsoudersjr@gmail.com>
2024-04-15 15:57:04 +0900

    Improve search and filter capability

--- src/lib/components/index/ExerciseIndexCard.svelte
+++ src/lib/components/index/ExerciseIndexCard.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import {thinkingEmoji} from "$lib/emojis";
+
 	import {EExerciseNames} from "../../../enums/exerciseNames";
 
 	import type {TExercise} from "../../../types/exercise";
@@ -13,11 +15,13 @@
   <h1 class="oswald-header">{exerciseName.toUpperCase()}</h1>
   {#if exercise.demos}
     {#each exercise.demos as demo (demo)}
+      <span class="demo">Demo:
       {#if  demo === ''}
-        <p>Coming Soon!</p>
+        <p>{thinkingEmoji}</p>
       {:else}
-        <a href={demo}>Demo link</a>
+        <a href={demo}>link</a>
       {/if}
+      </span>
     {/each}
   {/if}
 </div>
@@ -28,6 +32,13 @@
     border-radius: 8px;
     border: 1px solid #259259;
     padding: 5px 10px 
+  }
+  .demo {
+    display: grid;
+    grid-template-columns: repeat(2, min-content);
+    grid-template-rows: min-content;
+    align-items: center;
+    column-gap: 0.25rem;
   }
 </style>
 
--- src/routes/index/+page.svelte
+++ src/routes/index/+page.svelte
@@ -1,17 +1,62 @@
 <script lang="ts">
   import type { PageData } from "./$types";
+  import type { TExercisePosition } from "../../types/exercisePosition";
   import SearchBar from "$lib/components/index/SearchBar.svelte";
   import ExerciseIndexCard from "$lib/components/index/ExerciseIndexCard.svelte";
 	import FilterSection from "$lib/components/index/FilterSection.svelte";
+	import type {TExerciseCategory} from "../../types/exerciseCategory";
+	import type {TExerciseVariation} from "../../types/exerciseVariation";
+	import type {TBodyPart} from "../../types/bodyPart";
 
+  let exerciseNameSearchString: "";
   function handleSearchChange(event: CustomEvent) {
     exerciseList = exercises.filter((exercise) => {
-      return exercise.name?.includes(event.detail.value.toUpperCase())
+      exerciseNameSearchString = event.detail.value.toUpperCase();
+      return exercise.name?.includes(exerciseNameSearchString);
     });
   };
 
   function handleFilterUpdate(event: CustomEvent) {
-    console.log(event.detail);
+    const filters = event.detail.filter;
+    const checkedPositions = filters.position as TExercisePosition[];
+    const checkedCategories = filters.category as TExerciseCategory[];
+    const checkedVariations = filters.variation as TExerciseVariation[];
+    const checkedTargets = filters.target as TBodyPart[];
+
+    if (
+      checkedTargets.length === 0 &&
+      checkedCategories.length === 0 &&
+      checkedPositions.length === 0 &&
+      checkedVariations.length === 0
+    ) {
+      return exerciseList = exercises.filter((exercise) => {
+        return exercise.name?.includes(exerciseNameSearchString);
+      });
+    };
+
+    exerciseList = exercises.filter(( exercise ) => {
+      const exercisePositions = exercise.positions as TExercisePosition[];
+      const exerciseCategories = exercise.categories as TExerciseCategory[];
+      const exerciseVariations = exercise.variations as TExerciseVariation[];
+      const exerciseTargets = exercise.bodyParts as TBodyPart[];
+
+      for (let position of checkedPositions) {
+        if (exercisePositions.includes(position)) return true;
+      };
+
+      for (let variation of checkedVariations) {
+        if (exerciseVariations.includes(variation)) return true;
+      };
+
+      for (let category of checkedCategories) {
+        if (exerciseCategories.includes(category)) return true;
+      };
+
+      for (let target of checkedTargets) {
+        if (exerciseTargets.includes(target)) return true;
+      };
+
+    });
   }
 
   export let data: PageData;
@@ -23,6 +68,7 @@
   <SearchBar
     on:searchingForExercise={handleSearchChange}
   />
+  {exerciseList.length}
   <details>
     <summary class="oswald-header">FILTERS:</summary>
     <FilterSection on:filterUpdate={handleFilterUpdate} />
@@ -39,7 +85,7 @@
     height: 100%;
     display: grid;
     grid-template-columns: 1fr;
-    grid-template-rows: auto;
+    grid-template-rows: repeat(3, min-content);
     row-gap: 2rem;
 
     overflow-x: hidden;
