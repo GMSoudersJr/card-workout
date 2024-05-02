206b2fc6fbea0d4cae1c94cccf33eda1d78f5e86
Gerald M. Souders Jr <gmsoudersjr@gmail.com>
2024-05-03 08:08:27 +0900

    Update workout decks

--- src/lib/workoutsDB.ts
+++ src/lib/workoutsDB.ts
@@ -62,59 +62,59 @@ export const workouts: Workout[] = [
     ]
   },
   {
+    name: 'THE GARRETT',
+    description: "Dynamic legs, core, and arms",
+    suitExercises: [
+      {
+        suit: 'CLUBS',
+        exercise: exercises.filter(e => e.name === 'LEG_RAISES')[0],
+        completedReps: 0
+      },
+      {
+        suit: 'DIAMONDS',
+        exercise: exercises.filter(e => e.name === 'LUNGES')[0],
+        completedReps: 0
+      },
+      {
+        suit: 'HEARTS',
+        exercise: exercises.filter(e => e.name === 'PUSH_UPS')[0],
+        completedReps: 0
+      },
+      {
+        suit: 'SPADES',
+        exercise: exercises.filter(e => e.name === 'SQUATS')[0],
+        completedReps: 0
+      },
+    ]
+  },
+  {
+    name: 'THE INSKIP',
+    description: "Rotation, stability, and strength.",
+    suitExercises: [
+      {
+        suit: 'CLUBS',
+        exercise: exercises.filter(e => e.name === 'TRUNK_TWISTS')[0],
+        completedReps: 0
+      },
+      {
+        suit: 'DIAMONDS',
+        exercise: exercises.filter(e => e.name === 'MOUNTAIN_CLIMBERS')[0],
+        completedReps: 0
+      },
+      {
+        suit: 'HEARTS',
+        exercise: exercises.filter(e => e.name === 'PUSH_UPS')[0],
+        completedReps: 0
+      },
+      {
+        suit: 'SPADES',
+        exercise: exercises.filter(e => e.name === 'LUNGES')[0],
+        completedReps: 0
+      },
+    ]
+  },
+  {
     name: 'FULL BODY III',
-    description: "No equipment, calisthenics staples",
-    suitExercises: [
-      {
-        suit: 'CLUBS',
-        exercise: exercises.filter(e => e.name === 'CRUNCHES')[0],
-        completedReps: 0
-      },
-      {
-        suit: 'DIAMONDS',
-        exercise: exercises.filter(e => e.name === 'JUMPING_JACKS')[0],
-        completedReps: 0
-      },
-      {
-        suit: 'HEARTS',
-        exercise: exercises.filter(e => e.name === 'PUSH_UPS')[0],
-        completedReps: 0
-      },
-      {
-        suit: 'SPADES',
-        exercise: exercises.filter(e => e.name === 'ATG_SQUATS')[0],
-        completedReps: 0
-      },
-    ]
-  },
-  {
-    name: 'FULL BODY IV',
-    description: "No equipment, calisthenics staples",
-    suitExercises: [
-      {
-        suit: 'CLUBS',
-        exercise: exercises.filter(e => e.name === 'CRUNCHES')[0],
-        completedReps: 0
-      },
-      {
-        suit: 'DIAMONDS',
-        exercise: exercises.filter(e => e.name === 'JUMPING_JACKS')[0],
-        completedReps: 0
-      },
-      {
-        suit: 'HEARTS',
-        exercise: exercises.filter(e => e.name === 'PUSH_UPS')[0],
-        completedReps: 0
-      },
-      {
-        suit: 'SPADES',
-        exercise: exercises.filter(e => e.name === 'ATG_SQUATS')[0],
-        completedReps: 0
-      },
-    ]
-  },
-  {
-    name: 'FULL BODY V',
     description: "No equipment, calisthenics staples",
     suitExercises: [
       {
