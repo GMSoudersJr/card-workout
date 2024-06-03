af4a78f78ab2ed7773d75c571f969e505d40e5de
Gerald M. Souders Jr <gmsoudersjr@gmail.com>
2024-06-03 15:39:44 +0900

    feat(error-page): style the base error page
    Styled to base error page a bit

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,10 +1,31 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import { thinkingEmoji } from '$lib/emojis';
+
+	console.log('index', $page);
 </script>
 
-<h1>
-	{$page.error?.message}
-</h1>
+<div class="page-container">
+	<h1 class="noto-emoji-font">{thinkingEmoji}</h1>
+	<h1 class="oswald-header">{$page.status}: {$page.error?.message.toUpperCase()}</h1>
+	<h2>
+		<a href="/" class="oswald-header"> Go Home </a>
+	</h2>
+</div>
 
 <style>
+	.page-container {
+		display: grid;
+		height: 100%;
+		grid-template-columns: 100%;
+		grid-template-rows: repeat(auto-fill, min-content);
+		row-gap: 2rem;
+		justify-items: center;
+		align-content: center;
+		background-color: #000080;
+	}
+	h1,
+	a {
+		color: white;
+	}
 </style>
